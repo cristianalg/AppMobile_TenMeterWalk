031fd275896d9383b89a105df690a83df1a06910
cristianalg <cristianalg7@hotmail.com>
2023-05-30 15:25:28 +0100

    Responsive Design Login & Register App name = Walk Test

--- app/src/main/java/com/example/appmobile_tenmeterwalk/MainActivity.java
+++ app/src/main/java/com/example/appmobile_tenmeterwalk/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.appmobile_tenmeterwalk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView createnewAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createnewAccount = findViewById(R.id.createNewAccount);
+
+        createnewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+
     }
 }
